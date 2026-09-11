d25daec72b760d6c253565975711825fc79da6c3
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-09-11 12:15:47 -0700

    Added ZipperRevealApp entry point for the SwiftUI zipper reveal animation project.

--- ZipperReveal/ZipperRevealApp.swift
+++ ZipperReveal/ZipperRevealApp.swift
@@ -2,16 +2,17 @@
 //  ZipperRevealApp.swift
 //  ZipperReveal
 //
-//  Created by Z.K   on 11/09/2026.
+//  Created by Z.K  on 11/09/2026.
 //
 
 import SwiftUI
 
 @main
 struct ZipperRevealApp: App {
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZipperRevealView()
         }
     }
 }
---
+++ ZipperReveal/ZipperRevealView.swift
@@ -0,0 +1,8 @@
+//
+//  ZipperRevealView.swift
+//  ZipperReveal
+//
+//  Created by Z.K   on 11/09/2026.
+//
+
+
